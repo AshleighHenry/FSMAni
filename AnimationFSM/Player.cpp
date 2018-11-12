0a4a5b828f09ec1480f3c7a6444034bd4c332fd6
AshleighHenry <34344066+AshleighHenry@users.noreply.github.com>
2018-11-12 11:01:32 +0000

    Update AnimationFSM/Player.cpp

--- AnimationFSM/Player.cpp
+++ AnimationFSM/Player.cpp
@@ -33,20 +33,22 @@ void Player::handleInput(Input in)
 	case 0:
 		m_animation.idle();		
 		break;
-	case 1:
+	case 1: // down arrow
 		m_animation.shoveling();
-		m_animated_sprite.setStartAndEnd(13, 19);
+		m_animated_sprite.setStartAndEnd(18, 24);
+		//m_animated_sprite.setStartAndEnd(12, 18);
 		break;
-	case 2:
+	case 2: // right arrow
 		m_animation.walking();	
 		m_animated_sprite.setStartAndEnd(33, 39);
 		break;
-	case 3:		
+	case 3:		// up arrow
 		m_animation.climbing();
-		m_animated_sprite.setStartAndEnd(19, 25);
+		m_animated_sprite.setStartAndEnd(12, 18);
 		break;
 	case 4:
 		m_animation.jumping();
+		m_animated_sprite.setStartAndEnd(7, 13);
 		break;
 	case 5:
 		m_animation.swordsmanship();
